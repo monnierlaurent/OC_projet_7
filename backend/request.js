4e8fc3539c69d11f8ac0a2d7e6cde053c0e6f3f2
monnierlaurent <laurent.monnier51@orange.fr>
2020-10-29 18:43:58 +0100

    mise en place du package dotenv

--- backend/request.js
+++ backend/request.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
+
 const mysql = require('mysql');
 
 
 const conn = mysql.createConnection({
-    database: 'groupomania',
-    host: "localhost",
-    user: "root",
-    password: "Leane140304",
-    port: '3400'
+    database: process.env.DATABASE,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 });
 conn.connect(function(err) {
     if (err) throw err;
