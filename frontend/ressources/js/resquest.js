6b30873814175b0a14eca3b4c236d484a4c535bb
monnierlaurent <laurent.monnier51@orange.fr>
2020-11-03 14:15:40 +0100

    creation des requestes DELETE et PUT sur fichier request.js

--- frontend/ressources/js/resquest.js
+++ frontend/ressources/js/resquest.js
@@ -1,14 +1,13 @@
 const recupStorageAuth = sessionStorage.getItem('repAuth');
 const recupUserId = JSON.parse(recupStorageAuth);
 
-
 //function methode GET  (recuperation sans token)
-//console.log(recupUserId.token);
 async function request(url) {
     let response = await fetch(url);
     let data = await response.json();
     return data;
 };
+
 //function methode GET  (recuperation avec token)
 async function requestAuth(url) {
     let response = await fetch(url, {
@@ -19,7 +18,7 @@ async function requestAuth(url) {
     return data;
 };
 
-// function method post (envoie sans token)
+// function method POST (envoie sans token)
 async function send(url, datas) {
     let response = await fetch(url, {
         method: 'POST',
@@ -30,12 +29,25 @@ async function send(url, datas) {
     return data2;
 };
 
-// function method post (envoie avec token)
+// function method POST (envoie avec token) JSON
+async function send(url, datas) {
+    let response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer' + ' ' + recupUserId.token
+        },
+        body: JSON.stringify(datas)
+    });
+    let data2 = await response.json();
+    return data2;
+};
+
+// function method POST (envoie avec token) FORMDATA
 async function sendAuth(url, data) {
     let response = await fetch(url, {
         method: 'POST',
         headers: {
-
             'Authorization': 'Bearer' + ' ' + recupUserId.token
         },
         body: data
@@ -44,12 +56,22 @@ async function sendAuth(url, data) {
     return data3;
 };
 
-
+// function method DELETE (envoie avec token)
 async function deleteAuth(url) {
     let response = await fetch(url, {
         method: 'DELETE',
         headers: {
-            'Content-Type': 'application/json',
+            'Authorization': 'Bearer' + ' ' + recupUserId.token
+        }
+    });
+    let data = await response.json();
+    return data;
+};
+// function method PUT (envoie avec token)
+async function putAuth(url) {
+    let response = await fetch(url, {
+        method: 'PUT',
+        headers: {
             'Authorization': 'Bearer' + ' ' + recupUserId.token
         }
     });
